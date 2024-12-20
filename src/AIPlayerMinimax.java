0e2a5dcef95a75b7fd789b0761c570961ba8dbaf
Marcoindrajaya21 <162570609+Marcoindrajaya21@users.noreply.github.com>
2024-12-20 22:28:02 +0700

    Update AIPlayerMinimax.java

--- src/AIPlayerMinimax.java
+++ src/AIPlayerMinimax.java
@@ -1,5 +1,16 @@
 import java.util.*;
 
+/**
+ * ES234317-Algorithm and Data Structures
+ * Semester Ganjil, 2024/2025
+ * Group Capstone Project
+ * Group #1
+ * 1 - 5026231019 - Nathaniel Lado Hadi Winata
+ * 2 - 5026231031 - Marco Indrajaya
+ */
+
+
+
 /** AIPlayer using Minimax algorithm */
 public class AIPlayerMinimax extends AIPlayer {
 

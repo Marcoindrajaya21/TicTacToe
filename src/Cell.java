69c034066bf4a5264dc29ceb973aee99f10d8e0d
Marcoindrajaya21 <162570609+Marcoindrajaya21@users.noreply.github.com>
2024-12-20 22:28:48 +0700

    Update Cell.java

--- src/Cell.java
+++ src/Cell.java
@@ -1,6 +1,17 @@
 /**
  * The Cell class models each individual cell of the TTT 3x3 grid.
  */
+
+/**
+ * ES234317-Algorithm and Data Structures
+ * Semester Ganjil, 2024/2025
+ * Group Capstone Project
+ * Group #1
+ * 1 - 5026231019 - Nathaniel Lado Hadi Winata
+ * 2 - 5026231031 - Marco Indrajaya
+ */
+
+
 public class Cell {  // save as "Cell.java"
     // Define properties (package-visible)
     /** Content of this cell (CROSS, NOUGHT, EMPTY) */

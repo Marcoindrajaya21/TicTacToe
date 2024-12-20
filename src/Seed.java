a3eaf3e6f9a953e7f4cdbd1249c1b4ae69af8cb0
Marcoindrajaya21 <162570609+Marcoindrajaya21@users.noreply.github.com>
2024-12-20 22:29:09 +0700

    Update Seed.java

--- src/Seed.java
+++ src/Seed.java
@@ -1,6 +1,17 @@
 /**
  * Enumeration for the seeds and cell contents
  */
+
+/**
+ * ES234317-Algorithm and Data Structures
+ * Semester Ganjil, 2024/2025
+ * Group Capstone Project
+ * Group #1
+ * 1 - 5026231019 - Nathaniel Lado Hadi Winata
+ * 2 - 5026231031 - Marco Indrajaya
+ */
+
+
 public enum Seed {
     EMPTY, CROSS, NOUGHT
 }

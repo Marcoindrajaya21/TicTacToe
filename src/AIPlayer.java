24738801db51f1b9bef651eaf12b76aca8e17de4
Marcoindrajaya21 <162570609+Marcoindrajaya21@users.noreply.github.com>
2024-12-20 22:27:44 +0700

    Update AIPlayer.java

--- src/AIPlayer.java
+++ src/AIPlayer.java
@@ -1,6 +1,17 @@
 /**
  * Abstract superclass for all AI players with different strategies.
  */
+
+/**
+ * ES234317-Algorithm and Data Structures
+ * Semester Ganjil, 2024/2025
+ * Group Capstone Project
+ * Group #1
+ * 1 - 5026231019 - Nathaniel Lado Hadi Winata
+ * 2 - 5026231031 - Marco Indrajaya
+ */
+
+
 public abstract class AIPlayer {
     protected int ROWS = 3;  // number of rows
     protected int COLS = 3;  // number of columns

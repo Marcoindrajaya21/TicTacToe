2ed5edd2c947b352bc13eb20bc4c4b4c059d97c8
Marcoindrajaya21 <162570609+Marcoindrajaya21@users.noreply.github.com>
2024-12-20 22:29:36 +0700

    Update State.java

--- src/State.java
+++ src/State.java
@@ -1,6 +1,17 @@
 /**
  *  The enum State defines the various game states of the TTT game
  */
+
+/**
+ * ES234317-Algorithm and Data Structures
+ * Semester Ganjil, 2024/2025
+ * Group Capstone Project
+ * Group #1
+ * 1 - 5026231019 - Nathaniel Lado Hadi Winata
+ * 2 - 5026231031 - Marco Indrajaya
+ */
+
+
 public enum State {  // to save as "State.java"
     PLAYING, DRAW, CROSS_WON, NOUGHT_WON
 }
